fceee12591b0a5c8e3de930732d0acad15639c1a
王丽 <93381137+2388099717@users.noreply.github.com>
2021-10-29 20:42:53 +0800

    Add files via upload

---
+++ char与int关系.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int c1,c2;
+	printf("请输入两个整数c1,c2:");
+	scanf("%d,%d",&c1,&c2);
+	printf("按字符输入结果:\n");
+	printf("按ASCII码输入出结果为:\n");
+	printf("%d,%d\n",c1,c2);
+	return 0;
+}
---
+++ p25页.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+#define M 1000
+int main()
+{
+	int i,k;
+	printf("请输入一个小于%d的整数i:",M);
+	scanf("%d",&i);
+	if(i>M)
+	{printf("输入的数据不符合要求，请重新输入一个小于%d的整数i:",M);
+	scanf("%d",&i);
+	}
+	k=sqrt(i);
+	printf("%d的平方根的整数怒分是%d\n",i,k);
+	return 0;
+}
